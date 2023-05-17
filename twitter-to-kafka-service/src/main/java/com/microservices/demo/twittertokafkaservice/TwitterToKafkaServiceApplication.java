69a0be379a1c401419bfc653ca578239992e57c3
Cuma Cakmak <cumacakmak@gmail.com>
2023-05-17 16:02:15 +0300

    Add logger and config data

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/TwitterToKafkaServiceApplication.java
@@ -1,17 +1,32 @@
 package com.microservices.demo.twittertokafkaservice;
 
+import com.microservices.demo.twittertokafkaservice.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private final static Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("App starts");
+        LOG.info("App starts");
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+        LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
     }
 }
