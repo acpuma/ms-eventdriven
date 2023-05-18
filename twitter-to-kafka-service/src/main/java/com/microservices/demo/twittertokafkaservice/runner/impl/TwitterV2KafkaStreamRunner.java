8944f9e71546c8b2b676a5cb6f7f9bead3351e38
Cuma Cakmak <cumacakmak@gmail.com>
2023-05-18 23:10:00 +0300

    Twitter API V2

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/runner/impl/TwitterV2KafkaStreamRunner.java
@@ -0,0 +1,53 @@
+package com.microservices.demo.twittertokafkaservice.runner.impl;
+
+import com.microservices.demo.twittertokafkaservice.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twittertokafkaservice.runner.StreamRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConditionalOnExpression("${twitter-to-kafka-service.enable-v2-tweets} && not ${twitter-to-kafka-service.enable-mock-tweets}")
+public class TwitterV2KafkaStreamRunner implements StreamRunner {
+    private final Logger LOG = LoggerFactory.getLogger(TwitterV2KafkaStreamRunner.class);
+    private final TwitterToKafkaServiceConfigData configData;
+    private final TwitterV2StreamHelper twitterV2StreamHelper;
+
+    public TwitterV2KafkaStreamRunner(TwitterToKafkaServiceConfigData configData, TwitterV2StreamHelper twitterV2StreamHelper) {
+        this.configData = configData;
+        this.twitterV2StreamHelper = twitterV2StreamHelper;
+    }
+
+    @Override
+    public void start() {
+        String bearerToken = configData.getTwitterV2BearerToken();
+        if (null!= bearerToken) {
+            try {
+                LOG.info("BEARER : " + bearerToken);
+                twitterV2StreamHelper.setupRules(bearerToken, getRules());
+                twitterV2StreamHelper.connectStream(bearerToken);
+            } catch (IOException | URISyntaxException e) {
+                LOG.error("Error getting bearer token");
+                throw new RuntimeException("Error getting bearer token");
+            }
+
+        }
+    }
+
+    private Map<String, String> getRules() {
+        List<String> keywords = configData.getTwitterKeywords();
+        Map<String, String> rules = new HashMap<>();
+        for (String keyword:keywords) {
+            rules.put(keyword, "Keyword: " + keyword);
+        }
+        LOG.info("Created filter for twitter stream for keywords: {}", keywords);
+        return rules;
+    }
+}
