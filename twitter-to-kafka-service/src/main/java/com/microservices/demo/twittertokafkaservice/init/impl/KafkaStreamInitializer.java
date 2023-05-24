d80200c6659abe8aa6fd7eb9b2257055aee407e6
Cuma Cakmak <cumacakmak@gmail.com>
2023-05-24 23:52:38 +0300

    Kafka Producer part 2

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/init/impl/KafkaStreamInitializer.java
@@ -0,0 +1,28 @@
+package com.microservices.demo.twittertokafkaservice.init.impl;
+
+import com.microservices.demo.config.KafkaConfigData;
+import com.microservices.demo.kafka.admin.client.KafkaAdminClient;
+import com.microservices.demo.twittertokafkaservice.init.StreamInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaStreamInitializer implements StreamInitializer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaStreamInitializer.class);
+    private final KafkaConfigData kafkaConfigData;
+    private final KafkaAdminClient kafkaAdminClient;
+
+    public KafkaStreamInitializer(KafkaConfigData kafkaConfigData, KafkaAdminClient kafkaAdminClient) {
+        this.kafkaConfigData = kafkaConfigData;
+        this.kafkaAdminClient = kafkaAdminClient;
+    }
+
+    @Override
+    public void init() {
+        kafkaAdminClient.createTopics();
+        kafkaAdminClient.checkSchemaRegistry();
+        log.info("Topics with name {} is ready for operations!", kafkaConfigData.getTopicNamesToCreate().toArray());
+    }
+}
